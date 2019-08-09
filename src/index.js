7f56bf5d13bffa8de34653df52910c1df032ce76
Amuou <faliyarthur@gmail.com>
2019-08-09 13:19:18 +0200

    GenDiff refactoring

--- src/index.js
+++ src/index.js
@@ -1,53 +1,67 @@
 import fs from 'fs'
-import { has, compact } from 'lodash'
+import { has } from 'lodash'
 
-const getMissingEntries = (first, second) =>
-  Object.entries(first).filter(([key]) => !has(second, key))
+const types = [
+  {
+    name: 'changed',
+    check: ([key, value], second) => has(second, key) && value !== second[key],
+  },
+  {
+    name: 'unchanged',
+    check: ([key, value], second) => has(second, key) && value === second[key],
+  },
+  {
+    name: 'deleted',
+    check: ([key], second) => !has(second, key),
+  },
+]
 
-const getChangedEntries = (first, second) =>
-  Object.entries(first).reduce(
-    (acc, [key, value]) =>
-      has(second, key) && value !== second[key]
-        ? [
-            ...acc,
-            {
-              before: { operator: '-', key, value },
-              after: { operator: '+', key, value: second[key] },
-            },
-          ]
-        : acc,
-    [],
-  )
+const operators = {
+  added: '+',
+  deleted: '-',
+  unchanged: ' ',
+}
 
-const getRemainEntries = (first, second) =>
-  Object.entries(first).filter(
-    ([key, value]) => has(second, key) && value === second[key],
-  )
+const getType = (first, second) =>
+  types.find(({ check }) => check(first, second))
 
-const reverseAttrs = f => (a, b) => f(b, a)
+const getDiff = (first, second) => {
+  const typesWithoutAdded = Object.entries(first).map(elem => [
+    getType(elem, second).name,
+    ...elem,
+  ])
+  const typesDiff = Object.entries(second)
+    .reduce(
+      (acc, [key, value]) =>
+        !has(first, key) ? [...acc, ['added', key, value]] : acc,
+      typesWithoutAdded,
+    )
+    .slice()
+    .sort()
+
+  return typesDiff
+}
 
 const genDiff = (firstConfig, secondConfig) => {
   const first = JSON.parse(fs.readFileSync(firstConfig, 'utf8'))
   const second = JSON.parse(fs.readFileSync(secondConfig, 'utf8'))
-  const deleted = getMissingEntries(first, second)
-  const added = reverseAttrs(getMissingEntries)(first, second)
-  const changed = getChangedEntries(first, second)
-  const remain = getRemainEntries(first, second)
-  const diff = compact([
-    `${remain.map(([key, value]) => `    ${key}: ${value}`).join('\n')}`,
-    `${changed
-      .map(({ before, after }) =>
-        [
-          `  ${before.operator} ${before.key}: ${before.value}`,
-          `  ${after.operator} ${after.key}: ${after.value}`,
-        ].join('\n'),
+  const parseDiff = diff =>
+    diff
+      .reduce(
+        (acc, [type, key, value]) =>
+          type === 'changed'
+            ? acc.concat(
+                `  - ${key}: ${value}\n`,
+                `  + ${key}: ${second[key]}\n`,
+              )
+            : acc.concat(`  ${operators[type]} ${key}: ${value}\n`),
+        '',
       )
-      .join('\n')}`,
-    `${deleted.map(([key, value]) => `  - ${key}: ${value}`).join('\n')}`,
-    `${added.map(([key, value]) => `  + ${key}: ${value}`).join('\n')}`,
-  ]).join('\n')
+      .trimRight()
 
-  return `{\n${diff}\n}`
+  const result = getDiff(first, second) |> parseDiff
+
+  return `{\n${result}\n}`
 }
 
 export default genDiff
